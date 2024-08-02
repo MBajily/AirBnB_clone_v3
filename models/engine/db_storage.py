8ae705e164a4b7eb3ca270345ba7bd510f9d79f4
Mohammed Omar <mbajily@gmail.com>
2024-08-02 04:03:07 +0300

    2. Improve storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,7 +75,7 @@ class DBStorage:
         """call remove() method on the private session attribute"""
         self.__session.remove()
 
-def get(self, cls, id):
+    def get(self, cls, id):
         """
         Retrieves one object based on the class and its ID
         """
