22e6da73f6b1a300bd20042c88c92a0da751ea63
root <mbajily@gmail.com>
2024-08-05 04:27:40 -0700

    3. Status of your API

--- models/base_model.py
+++ models/base_model.py
@@ -23,8 +23,8 @@ class BaseModel:
     """The BaseModel class from which future classes will be derived"""
     if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
-        created_at = Column(DateTime, default=datetime.utcnow)
-        updated_at = Column(DateTime, null=True)
+        created_at = Column(DateTime, default=sqlalchemy.func.current_timestamp())
+        updated_at = Column(DateTime, default=sqlalchemy.func.current_timestamp(), onupdate=sqlalchemy.func.current_timestamp())
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
